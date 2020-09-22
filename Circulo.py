13a79cdc9a5685ae6a939e4529feea202233988a
LadySalazar <lady.salazar01@correo.usa.edu.co>
2020-09-22 12:30:30 -0500

    Circulo Completo

--- Circulo.py
+++ Circulo.py
@@ -12,3 +12,14 @@ class Circulo(Figura):
     def hallarArea(self):
         totalArea= math.pi * self.radio ** 2
         return round(totalArea)
+
+    def hallarPerimetro(self):
+        totalPerimetro= 2 * math.pi * self.radio
+        return round(totalPerimetro)
+    def DeterminarSiHayPunto(self,p):
+        distancia1 = p.hallarDistancia(self.centro)
+        if distancia1<=self.radio:
+            respuesta = True;
+        else:
+            respuesta = False;
+        return (respuesta)
--- test_Circulo.py
+++ test_Circulo.py
@@ -10,3 +10,14 @@ class TestCirculo(TestCase):
         c = Circulo(a,5)
         dist = c.hallarArea()
         self.assertEqual(dist,79)
+    def test_hallar_perimetro(self):
+        b = Punto(0,0)
+        c2 = Circulo(b,5)
+        dist = c2.hallarPerimetro()
+        self.assertEqual(dist,31)
+    def test_determinar_punto(self):
+        b1 = Punto(-1, 0)
+        b2 = Punto(4,0)
+        c3 = Circulo(b1,4)
+        dist = c3.DeterminarSiHayPunto(b2)
+        self.assertEqual(dist,False)
