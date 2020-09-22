796e7a55c0b7a99b4fb884755e00f2b76707c767
LadySalazar <lady.salazar01@correo.usa.edu.co>
2020-09-22 10:24:58 -0500

    crear rama circulo

---
+++ test_Punto.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from Punto import Punto
+
+class TestPunto(TestCase):
+    def test_punto(self):
+        a = Punto(1, 0)
+        b = Punto(-1, 0)
+        dist = a.hallarDistancia(b)
+        self.assertEqual(dist,2)
